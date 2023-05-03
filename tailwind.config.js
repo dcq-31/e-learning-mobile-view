78e753c6fd223048424a2d63c17cf0da829cd1e2
Darián Capote <dariancapoteq@gmail.com>
2023-05-02 23:59:51 -0400

    Config color theme

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,29 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: {
+          DEFAULT: "#f1b14b",
+          50: "#fcefdb",
+          100: "#f9e0b7",
+          200: "#f7d093",
+          300: "#f5c881",
+          400: "#f2b95d",
+          500: "#f1b14b",
+          600: "#c18e3c",
+          700: "#a97c35",
+          800: "#795926",
+          900: "#60471e",
+        },
+        tahiti: {
+          light: "#67e8f9",
+          DEFAULT: "#06b6d4",
+          dark: "#0e7490",
+        },
+      },
+    },
   },
   plugins: [],
-}
-
+};
